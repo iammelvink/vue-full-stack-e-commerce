6cb912baae9cedb04041dbf841a0e2c909102ac2
Melvin Kisten <iammelvink@gmail.com>
2020-12-06 21:25:25 +0200

    NavBar implemented

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,42 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <NavBar />
+    <router-view />
   </div>
 </template>
 
-<style> // do NOT use scoped for global use
-  * {
-    box-sizing: border-box;
-    font-family: Arial;
-  }
+<script>
+import NavBar from "./components/NavBar.vue";
+export default {
+  name: "App",
+  components: {
+    NavBar,
+  },
+};
+</script>
 
-  #page-wrap {
-    margin: auto;
-    max-width: 800px;
-    min-height: 100vh;
-  }
+<style>
+/* do NOT use scoped for global use */
+* {
+  box-sizing: border-box;
+  font-family: Arial;
+}
 
-  button {
-    background-color: black;
-    border: none;
-    border-radius: 8px;
-    color: white;
-    cursor: pointer;
-    font-size: 16px;
-    font-weight: bold;
-    outline: 0;
-    padding: 16px;
-  }
+#page-wrap {
+  margin: auto;
+  max-width: 800px;
+  min-height: 100vh;
+}
+
+button {
+  background-color: black;
+  border: none;
+  border-radius: 8px;
+  color: white;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: bold;
+  outline: 0;
+  padding: 16px;
+}
 </style>
