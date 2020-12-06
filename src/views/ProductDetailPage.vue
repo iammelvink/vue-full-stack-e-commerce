ec1189944ba26cf4b7ac755dca4b0a38415689b2
Melvin Kisten <iammelvink@gmail.com>
2020-12-06 20:40:59 +0200

    ProductDetailPage implemented

--- src/views/ProductDetailPage.vue
+++ src/views/ProductDetailPage.vue
@@ -1,40 +1,58 @@
 <template>
-  <h1>This is the Products Detail Page</h1>
+  <div id="page-wrap">
+    <div id="img-wrap">
+      <img v-bind:src="product.imageUrl" />
+    </div>
+    <div id="product-details">
+      <h1>{{ product.name }}</h1>
+      <h3 id="price">R{{ product.price }}</h3>
+      <p>Average rating:{{ product.averageRating }}</p>
+      <button id="add-to-cart">Add to Cart</button>
+      <h4>Description</h4>
+      <p>{{ product.description }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
+import { products } from "../fake-data";
 export default {
-    name: 'ProductDetailPage',
+  name: "ProductDetailPage",
+  data() {
+    return {
+      product: products.find((p) => p.id === this.$route.params.id),
+    };
+  },
 };
 </script>
 
 <style scoped>
-  #page-wrap {
-    margin-top: 16px;
-    padding: 16px;
-    max-width: 600px;
-  }
+#page-wrap {
+  margin-top: 16px;
+  padding: 16px;
+  max-width: 600px;
+}
 
-  #img-wrap {
-    text-align: center;
-  }
+#img-wrap {
+  text-align: center;
+}
 
-  img {
-    width: 400px;
-  }
+img {
+  width: 400px;
+}
 
-  #product-details {
-    padding: 16px;
-    position: relative;
-  }
+#product-details {
+  padding: 16px;
+  position: relative;
+}
 
-  #add-to-cart {
-    width: 100%;
-  }
+#add-to-cart {
+  width: 100%;
+}
 
-  #price {
-    position: absolute;
-    top: 24px;
-    right: 16px;
-  }
+#price {
+  position: absolute;
+  top: 24px;
+  right: 16px;
+}
 </style>
