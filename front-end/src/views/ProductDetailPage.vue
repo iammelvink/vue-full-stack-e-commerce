2fc6fd4afcce53d8e4382d0f22a6b19bbb637f81
Melvin Kisten <iammelvink@gmail.com>
2020-12-07 23:37:48 +0200

    Item ALREADY in Cart working

--- front-end/src/views/ProductDetailPage.vue
+++ front-end/src/views/ProductDetailPage.vue
@@ -8,18 +8,29 @@
       <h1>{{ product.name }}</h1>
       <h3 id="price">R{{ product.price }}</h3>
       <p>Average rating:{{ product.averageRating }}</p>
+
       <!-- v-on:click invokes a desired method
       show only if item is NOT in cart -->
       <button
         id="add-to-cart"
         v-on:click="addToCart"
-        v-if="!showSuccessMessage"
+        v-if="!itemIsInCart && !showSuccessMessage"
       >
         Add to Cart
       </button>
+
       <!-- show only if item is in cart -->
-      <button id="add-to-cart" class="green-button" v-if="showSuccessMessage">
+      <button
+        id="add-to-cart"
+        class="green-button"
+        v-if="!itemIsInCart && showSuccessMessage"
+      >
         Successfully added item to cart
+      </button>
+
+      <!-- show only if item is ALREADY in cart -->
+      <button id="add-to-cart" class="grey-button" v-if="itemIsInCart">
+        Item is already in cart
       </button>
       <h4>Description</h4>
       <p>{{ product.description }}</p>
@@ -47,8 +58,24 @@ export default {
        * initialize to an empty object
        */
       product: {},
+      // initialize cart items as an empty array
+      cartItems: [],
+      /**
+       * success message for adding product to cart
+       * first starts as false
+       */
       showSuccessMessage: false,
     };
+  },
+  /**
+   * computed property
+   * Checks if product being added is equal to
+   * a product in the cart
+   */
+  computed: {
+    itemIsInCart() {
+      return this.cartItems.some((item) => item.id === this.product.id);
+    },
   },
   // to call methods from within vue template
   methods: {
@@ -74,12 +101,26 @@ export default {
    * when page loads
    */
   async created() {
-    // using back ticks for template strings
-    const result = await axios.get(`/api/products/${this.$route.params.id}`);
-    // loads product data from server
-    const product = result.data;
+    /**
+     * using back ticks for template strings
+     * loads product data from server
+     */
+    const { data: product } = await axios.get(
+      `/api/products/${this.$route.params.id}`
+    );
     // set product in vue component to product from server
     this.product = product;
+
+    /**
+     * loads cart items data from server
+     * this is for NOT hard coded user id
+     * using back ticks for template strings
+     * await axios.get(`/api/users/${this.$route.params.id}/cart`);
+     * notice user id is hard coded in this example
+     */
+    const { data: cartItems } = await axios.get(`/api/users/12345/cart`);
+    // set cart items in vue component to cart items from server
+    this.cartItems = cartItems;
   },
 };
 </script>
@@ -116,5 +157,8 @@ img {
 }
 .green-button {
   background-color: green;
+}
+.grey-button {
+  background-color: #888;
 }
 </style>
